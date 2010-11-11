d43aa2ee1b968e4287b5b6034eeba3e51f7427fa
flosmn <flosmn@gmail.com>
2010-11-11 17:00:03 +0100

    add AttributeModifierCounter

---
+++ javaprojectsources/project1/AttributeModifier.java
@@ -0,0 +1,11 @@
+public class AttributeModifier {
+	public double difference = 0.0;
+	private static int number = 0;
+	private volatile boolean bool = true;
+	
+	private synchronized int doNothing() { }
+	public static void main(String[] args) {
+		int i = 0;
+		System.out.println("HalloWelt");
+	}
+}
--- javaprojectsources/project2/HalloUniverse.java
+++ javaprojectsources/project2/HalloUniverse.java
@@ -1,4 +1,6 @@
 public class HalloUniverse {
+	private volatile boolean finished = false;
+	
 	public static void main(String[] args) {
 		System.out.println("HalloUniverse");
 	}
