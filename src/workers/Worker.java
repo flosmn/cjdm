d7012c7812fb9076ecd3fb3f5d3fd2663a541042
Christian Wellenbrock <christian.wellenbrock@gmail.com>
2010-11-11 16:03:50 +0100

    added comments to abstract worker

--- src/workers/Worker.java
+++ src/workers/Worker.java
@@ -7,11 +7,31 @@ import org.antlr.runtime.tree.CommonTree;
 
 import utils.DirtyLittleHelper;
 
+/**
+ * Extend this abstract class to implement a worker.
+ * Every worker computes the value for one attribute using a CommonTreePackage.
+ * @author welle
+ */
 public abstract class Worker {
-	public abstract int doWork(CommonTreePackage treePackage);
+	/**
+	 * getter for attribute name
+	 * @return the name of the computed attribute
+	 */
 	public abstract String getAttributeName();
 	
-	// util method for tree visitors
+	/**
+	 * computes the value
+	 * @param treePackage the CommonTreePackage to compute the value for
+	 * @return the value of the attribute for the given treePackage.
+	 */
+	public abstract int doWork(CommonTreePackage treePackage);
+	
+	/**
+	 * helper method to find the first child of a given type
+	 * @param parent the tree to scan the children of
+	 * @param type the type of the searched child
+	 * @return the first child of the given type or null if none is found
+	 */
 	public CommonTree firstChildWithType(CommonTree parent, String type) {
 		if (parent == null) {
 			return null;
