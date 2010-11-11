c3d063144ea36037b6cf5e236bf271e5115b9a1d
flosmn <flosmn@gmail.com>
2010-11-11 16:17:26 +0100

    refactor PathAndFileNames class

--- src/utils/PathAndFileNames.java
+++ src/utils/PathAndFileNames.java
@@ -3,7 +3,8 @@ package utils;
 public class PathAndFileNames {
 
 	public static final String PROJECT_SOURCES_PATH = "javaprojectsources/";
-	public static final String EXPORT_FILE_NAME = "cjdm";
+	public static final String RELATION_NAME = "cjdm";
+	public static final String EXPORT_FILE_NAME = RELATION_NAME + ".arff";
 	public static final String WEKA_TEST_DATA_PATH = "javaprojectsources/data/";
 	
 }
