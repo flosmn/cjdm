d11b85ba8df78d171df458f35381cec793a6e26d
flosmn <flosmn@gmail.com>
2010-11-21 12:57:26 +0100

    new worker: count nestedness of locks.

---
+++ src/workers/CountNestednessOfLocks.java
@@ -0,0 +1,79 @@
+package workers;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+
+import org.antlr.runtime.tree.CommonTree;
+import org.antlr.runtime.tree.Tree;
+
+import database.Aggregator;
+import database.Scope;
+
+import utils.DirtyLittleHelper;
+import main.TreePackage;
+
+public class CountNestednessOfLocks extends Worker{
+	private int maxNestedness;
+	private int nestedness;
+	private Collection<String> lockObjects;
+
+	public CountNestednessOfLocks() {
+		this.scope = Scope.METHOD;
+		this.aggregator = Aggregator.MAX;
+	}
+	
+	@Override
+	public String getAttributeName() {
+		return "nestedness_locks";
+	}
+	
+	public void traverse(CommonTree tree){
+		if(tree == null){
+			System.out.println("tree == null");
+			return;
+		}
+	
+		if (tree.getText() != null && tree.getText().equals(".")) {
+			Tree leftChild = tree.getChild(0);
+			Tree rightChild = tree.getChild(1);
+			
+			if(rightChild.getText().equals("lock")){
+				nestedness++;
+				lockObjects.add(leftChild.getText());
+				
+				if(nestedness > maxNestedness){
+					maxNestedness = nestedness;
+				}
+			}
+			
+			if(rightChild.getText().equals("unlock") && 
+					lockObjects.contains(leftChild.getText())){
+				nestedness--;
+				lockObjects.remove(leftChild.getText());
+			}
+		}
+		
+		List<CommonTree> children = DirtyLittleHelper.castList(CommonTree.class, tree.getChildren());
+		if (children == null) {
+			return;
+		}
+		
+		for (CommonTree child : children) {
+			traverse(child);
+		}
+	}
+
+	@Override
+	public int doWork(TreePackage treePackage) {
+		lockObjects = new HashSet<String>();
+		maxNestedness = 0;
+		nestedness = 0;
+		
+		traverse(treePackage.getTree());
+		
+		return maxNestedness;
+	}
+
+}
+
