c1dffcbb741c484e791e291d9fea4226951df097
Juergen Walter <juergen.walter.halle@gmail.com>
2010-12-09 16:42:46 +0100

    modified attributes strings to upper case
    Signed-off-by: Juergen Walter <juergen.walter.halle@gmail.com>

--- src/attributes/MethodAttributes.java
+++ src/attributes/MethodAttributes.java
@@ -1,29 +1,29 @@
 package attributes;
 
 public class MethodAttributes extends Attributes {
-	public static final String PUBLIC_METHODS = "public_methods";
-	public static final String PRIVATE_METHODS = "private_methods";
-	public static final String SYNCHRONIZED_METHODS = "synchronized_methods";
-	public static final String METHOD_CALLS = "method_calls";
-	public static final String NUMBER_OF_METHODS = "number_of_methods";
-	public static final String SYNCHRONIZED_BLOCKS = "synchronized_blocks";
-	public static final String LOCK_CALLS = "lock_calls";
-	public static final String UNLOCK_CALLS= "unlock_calls";
-	public static final String WAIT_CALLS = "wait_calls";
-	public static final String NOTIFY_CALLS = "notify_calls";
-	public static final String NOTIFYALL_CALLS = "notifyAll_calls";
-	public static final String SLEEP_CALLS = "sleep_calls";
-	public static final String YIELD_CALLS = "yield_calls";
-	public static final String JOIN_CALLS = "join_calls";
+	public static final String PUBLIC_METHODS = "PUBLIC_METHODS";
+	public static final String PRIVATE_METHODS = "PRIVATE_METHODS";
+	public static final String SYNCHRONIZED_METHODS = "SYNCHRONIZED_METHODS";
+	public static final String METHOD_CALLS = "METHOD_CALLS";
+	public static final String NUMBER_OF_METHODS = "NUMBER_OF_METHODS";
+	public static final String SYNCHRONIZED_BLOCKS = "SYNCHRONIZED_BLOCKS";
+	public static final String LOCK_CALLS = "LOCK_CALLS";
+	public static final String UNLOCK_CALLS= "UNLOCK_CALLS";
+	public static final String WAIT_CALLS = "WAIT_CALLS";
+	public static final String NOTIFY_CALLS = "NOTIFY_CALLS";
+	public static final String NOTIFYALL_CALLS = "NOTIFYALL_CALLS";
+	public static final String SLEEP_CALLS = "SLEEP_CALLS";
+	public static final String YIELD_CALLS = "YIELD_CALLS";
+	public static final String JOIN_CALLS = "JOIN_CALLS";
 	
-	public static final String LOCK_OBJECTS = "lock_objects";
-	public static final String COUNT_DOWN_OBJECTS = "countDownLatch_objects";
-	public static final String CONDITION_OBJECTS = "condition_objects";
-	public static final String SEMAPHORE_OBJECTS = "semaphore_objects";
-	public static final String CYCLICBARRIER_OBJECTS = "cyclicBarrier_objects";
+	public static final String LOCK_OBJECTS = "LOCK_OBJECTS";
+	public static final String COUNT_DOWN_OBJECTS = "COUNTDOWNLATCH_OBJECTS";
+	public static final String CONDITION_OBJECTS = "CONDITION_OBJECTS";
+	public static final String SEMAPHORE_OBJECTS = "SEMAPHORE_OBJECTS";
+	public static final String CYCLICBARRIER_OBJECTS = "CYCLICBARRIER_OBJECTS";
 	
-	public static final String NESTEDNESS_LOCKS = "nestedness_locks";
-	public static final String NESTEDNESS_SYNCHRONIZED = "nestedness_synchronized";
-	public static final String NESTEDNESS_CONDITIONALS = "nestedness_conditionals";
-	public static final String NESTEDNESS_LOOPS = "nestedness_loops";
+	public static final String NESTEDNESS_LOCKS = "NESTEDNESS_LOCKS";
+	public static final String NESTEDNESS_SYNCHRONIZED = "NESTEDNESS_SYNCHRONIZED";
+	public static final String NESTEDNESS_CONDITIONALS = "NESTEDNESS_CONDITIONALS";
+	public static final String NESTEDNESS_LOOPS = "NESTEDNESS_LOOPS";
 }
