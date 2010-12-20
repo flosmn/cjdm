05978aae186b826c50ac2b3a23fdc75a930e76f4
flosmn <flosmn@gmail.com>
2010-12-20 10:50:52 +0100

    remove double shutdown

--- src/r/DendogramCreator.java
+++ src/r/DendogramCreator.java
@@ -8,7 +8,7 @@ public class DendogramCreator {
 
 	public static void main(String[] args) {
         Rengine re=new Rengine(args, false, null);
-        // re.eval("source(\"" + PathAndFileNames.R_DATA_PATH + "projectscript.R\")");
+        re.eval("source(\"" + PathAndFileNames.R_DATA_PATH + "projectscript.R\")");
         re.eval("source(\"" + PathAndFileNames.R_DATA_PATH + "classscript.R\")");
         re.eval("source(\"" + PathAndFileNames.R_DATA_PATH + "methodscript.R\")");
         System.out.println("done");
