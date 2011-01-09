105f274d04832538425f213ff459f3da90bfdba8
flosmn <flosmn@gmail.com>
2011-01-09 19:50:32 +0100

    added colormatrix output to R-scripts

--- src/clustering/DendogramCreator.java
+++ src/clustering/DendogramCreator.java
@@ -8,8 +8,8 @@ public class DendogramCreator {
 
 	public static void main(String[] args) {
         Rengine re=new Rengine(args, false, null);
-        //re.eval("source(\"" + PathAndFileNames.R_DATA_PATH + "projectscript.R\")");
-        //re.eval("source(\"" + PathAndFileNames.R_DATA_PATH + "classscript.R\")");
+        re.eval("source(\"" + PathAndFileNames.R_DATA_PATH + "projectscript.R\")");
+        re.eval("source(\"" + PathAndFileNames.R_DATA_PATH + "classscript.R\")");
         re.eval("source(\"" + PathAndFileNames.R_DATA_PATH + "methodscript.R\")");
         System.out.println("done");
         System.exit(0);
--- src/r/RBridge.java
+++ src/r/RBridge.java
@@ -8,19 +8,29 @@ public class RBridge {
 	public static void main(String[] args) {
 	    Rengine re=new Rengine(args, false, null);
 	    REXP x;
-	    x = re.eval("read.table(\"javaprojectsources/csv/class.csv\",header=TRUE,sep=\",\")", false);
+	    x = re.eval("read.table(\"javaprojectsources/csv/project.csv\",header=TRUE,sep=\",\")", false);
 	    re.assign("data", x);
 	    
 	    x = re.eval("data[,1]", false);
 	    re.assign("rownames(data)", x);
-	    	
-	    x = re.eval("data[,1]", true);
-	    System.out.println(x);
 
 	    x = re.eval("data[,-1]", false);
 	    re.assign("data", x);
 
-	    x = re.eval("data");
+	    x = re.eval("scale(data)", false);
+	    re.assign("data", x);
+	    
+	    x = re.eval("dist(data, method=\"euclidean\")", false);
+	    re.assign("d", x);
+	    
+	    x = re.eval("hclust(d, method=\"ward\")", false);
+	    re.assign("fit", x);
+	    
+	    re.eval("pdf(file=\"javaprojectsources/r/project.pdf\", height=25, width=75)", false);
+	    re.eval("plot(fit)", false);
+	    re.eval("dev.off()", false);
+	    
+	    x = re.eval("fit");
 	    System.out.println(x);
 	    
 	    re.end();
